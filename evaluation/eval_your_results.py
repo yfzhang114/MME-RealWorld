92be4ce4c1e57260d16dc8bbb68be79150cd8a6b
yfzhang114 <yifanzhang.cs@gmail.com>
2024-09-20 10:05:27 +0000

    ourput overall acc

--- evaluation/eval_your_results.py
+++ evaluation/eval_your_results.py
@@ -4,7 +4,7 @@ import re
 import argparse
 from tqdm import tqdm 
 parser = argparse.ArgumentParser()
-parser.add_argument("--results_file", type=str, default='./result.jsonl')
+parser.add_argument("--results_file", type=str, default='')
 args = parser.parse_args()
 
 TASKS = [
@@ -16,7 +16,7 @@ SUBTASKS = [
     "Monitoring",
     "OCR with Complex Context",
     "Diagram and Table",
-    'Autonomous_Driving',
+    "Autonomous_Driving",
     'Remote Sensing'
 ]
 
@@ -29,7 +29,7 @@ def extract_characters_regex(s, choices):
         "The answer",
         "The best option is"
         "The correct option is",
-        "Best answer:",
+        "Best answer:"
         "Best option:",
     ]
     for answer_prefix in answer_prefixes:
@@ -47,6 +47,7 @@ def extract_characters_regex(s, choices):
 
 print(args.results_file)
 data = [json.loads(q) for q in open(os.path.expanduser(args.results_file), "r")]
+# 遍历所有问题
 cnt = 0
 
 results = {}
@@ -55,48 +56,60 @@ for task in TASKS:
     for subtask in SUBTASKS:
         results[f'{task}'][f'{subtask}'] = {}
         
-
 for question in tqdm(data):
     Task = question['Task']
     Subtask = question['Subtask']
-    Category = question['Category']
+    Category = question['Category'].lower()
     question_id = question["Question_id"]
     ground_truth = question["Ground truth"]
-    text = question["Output"]
+    text = question["output"]
+    
+    if 'attribute' in Category.lower():
+        Category = Category.split('/')[0] + '/attribute'
     
     text = extract_characters_regex(text, question['Answer choices'])
-    
+    # 检查 Ground Truth 和 text 是否相同
     cnt = ground_truth == text
     
     if Category not in results[Task][Subtask].keys():
-        results[Task][Subtask][f'{Category}'] = {'true': cnt, 'false': 1-cnt}
+        results[Task][Subtask][f'{Category}'] = {'true': cnt, 'false': 1 - cnt, 'is_E': text == 'E'}
     else:
         results[Task][Subtask][f'{Category}']['true'] += cnt
         results[Task][Subtask][f'{Category}']['false'] += 1 - cnt
+        results[Task][Subtask][f'{Category}']['is_E'] += text == 'E'
 
 
+
+sum_all, succ_all = 0, 0
 for task, tasks_values in results.items():
     print(f'*'*32 + f'{task} (Task Start)')
-    cnt_task, sum_task = 0, 0
+    cnt_task, cnt_E, sum_task = 0, 0, 0
     for substask, subtask_value in tasks_values.items():
         print(f'+'*16 + f'{substask} (Subtask Start)')
-        cnt_subtask, sum_subtask = 0, 0
+        cnt_subtask, sum_subtask, e_subtask = 0, 0, 0
         for category, category_dict in subtask_value.items():
             cnt_subtask += category_dict['true']
             sum_subtask += category_dict['false'] + category_dict['true']
+            e_subtask += category_dict['is_E']
             acc = category_dict['true'] / (category_dict['false'] + category_dict['true'])
-            print(f'-'*4 + f'\t' + 'Acc ' + '{:.4f}'.format(acc) + f'\t{category.capitalize()}')
+            print(f'-'*4 + f'\t' + 'Acc ' + '{:.4f}'.format(acc) + f"\t{category.capitalize()} ({category_dict['false'] + category_dict['true']} items)")
         
         if sum_subtask == 0:
             acc_subtasks = 0
+            e_subtask = 0
         else:
             acc_subtasks = cnt_subtask / sum_subtask
-        print(f'+'*16 + f'\t Acc ' + '{:.4f}'.format(acc_subtasks) + f'\t{substask}')
+        print(f'+'*16 + f'\t Acc ' + '{:.4f}'.format(acc_subtasks) + f'\t E choice {e_subtask} \t{substask} ({sum_subtask} items)')
         cnt_task += cnt_subtask
         sum_task += sum_subtask
+        cnt_E += e_subtask
     
     if sum_task == 0:
         acc_task = 0
     else:
         acc_task = cnt_task / sum_task
-    print(f'*'*32 + f'Acc' + '{:.4f}\n'.format(acc_task) + f'\t{task}')
+    succ_all += cnt_task
+    sum_all += sum_task
+    print(f'*'*32 + f'Acc ' + '{:.4f}'.format(acc_task) + f'\t E choice {cnt_E} \t{task} ({sum_task} items)\n')
+    
+print(f'*'*32 + f'Overall Acc ' + '{:.4f}'.format(succ_all / sum_all))
